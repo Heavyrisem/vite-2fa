d9e4065942de14fbbb0b9afd2d53fc958e1f5d43
pyo1748 <pyo1748@estsecurity.com>
2022-11-24 16:48:04 +0900

    테스트 API 추가, API token 연동

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import tw from 'twin.macro';
 
+import Button from '@components/Button';
 import DefaultLayout from '@components/Layouts/DefaultLayout';
 import authorizationState from '@recoil/atoms/authorization';
 import userSelector from '@recoil/selectors/user';
+import { axios } from '@utils/axios';
 
 const Home: React.FC = () => {
-  const authorization = useRecoilValue(authorizationState);
+  const [authorization, setAuthorization] = useRecoilState(authorizationState);
   const user = useRecoilValue(userSelector);
+
+  const [message, setMessage] = useState<string>();
+
+  const handleTestClick = useCallback(async () => {
+    setMessage(undefined);
+    const response = await axios.axiosInstance.get('/api/test').then((res) => res.data);
+    setMessage(response);
+  }, []);
 
   return (
     <DefaultLayout>
       <div css={[tw`text-4xl font-extrabold mb-8`]}>Home</div>
-      <div css={[tw`text-sm text-zinc-200`]}>
-        <pre>{JSON.stringify(user, null, 4)}</pre>
+      <div css={[tw`flex flex-col gap-4`]}>
+        <Button css={[tw`w-52`]} onClick={handleTestClick}>
+          AuthenticationTest
+        </Button>
+        {message && <div>{message}</div>}
+        <Button css={[tw`w-52`]} onClick={() => setAuthorization({ token: null })}>
+          Logout
+        </Button>
+        <div>UserDetail</div>
+        <div css={[tw`text-sm text-zinc-200`]}>
+          <pre>{JSON.stringify(user, null, 4)}</pre>
+        </div>
+        <div css={[tw`break-all text-xs text-zinc-600`]}>{authorization.token}</div>
       </div>
-      <div css={[tw`text-xs text-zinc-600`]}>{authorization.token}</div>
     </DefaultLayout>
   );
 };
--- src/recoil/atoms/authorization.ts
+++ src/recoil/atoms/authorization.ts
@@ -1,6 +1,7 @@
 import { atom } from 'recoil';
 
 import { localStorageEffect } from '@recoil/utils';
+import { axios } from '@utils/axios';
 
 const AUTHORIZATION_KEY = 'authorization';
 
@@ -8,12 +9,23 @@ export interface Authorization {
   token: string | null;
 }
 
+const defaultValue: Authorization = JSON.parse(localStorage.getItem(AUTHORIZATION_KEY) ?? '');
 const authorizationState = atom<Authorization>({
   key: 'authorization',
   default: {
-    token: localStorage.getItem(AUTHORIZATION_KEY),
+    token: null,
   },
-  effects: [localStorageEffect<Authorization>(AUTHORIZATION_KEY)],
+  effects: [
+    ({ onSet }) => {
+      if (defaultValue.token) axios.setAuthorization(defaultValue.token);
+      onSet((newValue) => {
+        if (newValue.token) {
+          axios.setAuthorization(newValue.token);
+        }
+      });
+    },
+    localStorageEffect<Authorization>(AUTHORIZATION_KEY),
+  ],
 });
 
 export default authorizationState;
